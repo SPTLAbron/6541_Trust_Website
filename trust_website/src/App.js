8e6bd44c7d596d1b092c4afb99acb798b4c78f09
Sai Pranav Theerthala <sptheerthala@gmail.com>
2023-07-15 10:42:58 -0400

    fist real changes

--- trust_website/src/App.js
+++ trust_website/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <div className="header">
+        <div className="title">6541 Trust</div>
+        <div className="scrolldown">
+          <div className="activites_page">Activities</div>
+          <div className="founders_page">Founders</div>
+          <div className="organizations_page">Participating Organizations</div>
+        </div>
+      </div>
     </div>
   );
 }
