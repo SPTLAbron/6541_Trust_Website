9c72a9af7349c6e35d6744f3ca6dd8f143573af1
KAbhjit <kavuru.abhijeet@gmail.com>
2023-08-10 07:18:22 +0530

    hi

--- trust_website/src/components/HomePage.js
+++ trust_website/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./HomePage.css";
 // import image from "./download.jpeg";
 
@@ -23,18 +24,39 @@ export default function HomePage() {
           </div>
         </div>
         {/* <div className="section_3">
-      <div className="contact_us_header">Contact Us</div>
-    </div>
-    <input type="text" onChange={onChange} /> */}
+          <div className="contact_us_header">Contact Us</div>
+        </div>
+        <input type="text" onChange={onChange} /> */}
       </div>
       <div className="bottom_section">
-        <div className="bottom_title">6541 Trust</div>
+        {/* Clickable Company Title */}
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+          <div className="bottom_title">6541 Trust</div>
+        </Link>
         <div className="bottom_content">
           <div className="bottom_statement">
             The 6541 works to provide crucial medicine and education for the
             needy.
           </div>
         </div>
+      </div>
+      {/* About Us Box */}
+      <div
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          backgroundColor: "#f0f0f0",
+          padding: "10px",
+          borderRadius: "8px",
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <p style={{ marginBottom: "5px" }}>
+          <strong>About Us</strong>
+        </p>
+        <p>WhatsApp: +123456789</p>
+        <p>Email: example@example.com</p>
       </div>
     </>
   );
