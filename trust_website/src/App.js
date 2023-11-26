eab659db2fb6bd744e5417777b0c809f511bfd3f
Sai Pranav Theerthala <sptheerthala@gmail.com>
2023-11-26 16:53:04 -0500

    made routing changes

--- trust_website/src/App.js
+++ trust_website/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import FoundersPage from "./components/FoundersPage";
 import BottomPart from "./components/BottomPart";
@@ -7,31 +7,46 @@ import HomePage from "./components/HomePage";
 import EducationPage from "./components/EducationPage";
 import MedicalPage from "./components/MedicalPage";
 
+// function App() {
+//   let Component;
+//   switch (window.location.pathname) {
+//     case "/":
+//       Component = <HomePage />;
+//       break;
+//     case "/founders":
+//       Component = <FoundersPage />;
+//       break;
+//     case "/medical":
+//       Component = <MedicalPage />;
+//       break;
+//     case "/education":
+//       Component = <EducationPage />;
+//       break;
+
+//     default:
+//       return null;
+//   }
+
+//   console.log(window.location);
+//   return (
+//     <Router>
+//       <NavBar />
+//       {Component}
+//       <BottomPart />
+//     </Router>
+//   );
+// }
+
 function App() {
-  let Component;
-  switch (window.location.pathname) {
-    case "/":
-      Component = <HomePage />;
-      break;
-    case "/founders":
-      Component = <FoundersPage />;
-      break;
-    case "/medical":
-      Component = <MedicalPage />;
-      break;
-    case "/education":
-      Component = <EducationPage />;
-      break;
-
-    default:
-      return null;
-  }
-
-  console.log(window.location);
   return (
     <Router>
       <NavBar />
-      {Component}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/founders" element={<FoundersPage />} />
+        <Route path="/medical" element={<MedicalPage />} />
+        <Route path="/education" element={<EducationPage />} />
+      </Routes>
       <BottomPart />
     </Router>
   );
